6a916a3ac631e68b696236b8713f950561b0dd69
Justin Welenofsky <welenofsky@gmail.com>
2016-10-02 00:01:59 -0700

    Check if site can be reached

--- upornot.rb
+++ upornot.rb
@@ -1,8 +1,13 @@
 require "socket"
 
+unless server = ARGV[0]
+	puts "Need to provide server address"
+	exit
+end
+
 begin
 	s = Socket.new Socket::AF_INET, Socket::SOCK_STREAM
-	s.connect Socket.pack_sockaddr_in(80, 'asdklfjasdljflkjfd.com')
+	s.connect Socket.pack_sockaddr_in(80, server)
 rescue SocketError
-	puts "Error connecting to host"
+	puts "Error connecting to host: #{server}"
 end
