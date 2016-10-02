10b261b956663f4ea59e2e39f3096aeec04994df
Justin Welenofsky <welenofsky@gmail.com>
2016-10-02 00:44:28 -0700

    Add timestamps

--- upornot.rb
+++ upornot.rb
@@ -5,9 +5,17 @@ unless server = ARGV[0]
 	exit
 end
 
-begin
-	s = Socket.new Socket::AF_INET, Socket::SOCK_STREAM
-	s.connect Socket.pack_sockaddr_in(80, server)
-rescue SocketError
-	puts "Error connecting to host: #{server}"
+def check_server(server)
+	begin
+		s = Socket.new Socket::AF_INET, Socket::SOCK_STREAM
+		s.connect Socket.pack_sockaddr_in(80, server)
+		puts "Online #{Time.now}"
+	rescue SocketError
+		puts "Offline: Error connecting to host: #{server} at #{Time.now}"
+	end
+	true
+end
+
+loop do
+	check_server(server) && sleep(30)
 end
